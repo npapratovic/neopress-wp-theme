cd157435994e217f37248f52e8f387e95a68debe
NIKOLA PAPRATOVIC <nikola.papratovic@gmail.com>
2022-09-02 16:25:34 +0200

    Update helper-functions.php

--- helper-functions.php
+++ helper-functions.php
@@ -181,4 +181,28 @@ function my_google_conversion(){
     }
     add_action( 'woocommerce_after_add_to_cart_button', 'add_content_after_addtocart' );
 
+/*===================================================*/ 
+
+/*===================================================*/
+
+/* 6) Hide ALL shipping rates in ALL zones when Free Shipping is available  */
+  
+add_filter( 'woocommerce_package_rates', 'bbloomer_unset_shipping_when_free_is_available_all_zones', 9999, 2 );
+   
+function bbloomer_unset_shipping_when_free_is_available_all_zones( $rates, $package ) {
+   $all_free_rates = array();
+   foreach ( $rates as $rate_id => $rate ) {
+      if ( 'free_shipping' === $rate->method_id ) {
+         $all_free_rates[ $rate_id ] = $rate;
+         break;
+      }
+   }
+   if ( empty( $all_free_rates )) {
+      return $rates;
+   } else {
+      return $all_free_rates;
+   } 
+}
+
+
 /*===================================================*/
