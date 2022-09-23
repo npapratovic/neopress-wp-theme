1404dd6b7eae22d8b6ad71ee0ac5a050b0990086
NIKOLA PAPRATOVIC <nikola.papratovic@gmail.com>
2022-09-23 09:52:34 +0200

    Create content-regular.php

---
+++ templates/content-regular.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+    <section class="inner-page page-content" id="about">
+        <div class="container">
+            <div class="row w-100 m-0">
+                <div class="col col-12 col-md-12 text-center">
+                    <h2 class="page-title"><?php echo get_the_title(); ?></h2>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row width-100 my-0">
+                <div class="col col-12 col-md-12 inner-page-content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
